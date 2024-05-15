a4afafe5f5e2ced4d7c64108477a7f46e94e198a
Valentim <timzejr2@gmail.com>
2024-05-14 22:38:35 -0300

    testes em endpoints

--- calculadora-para-teste/teste-calculadora/UnitTest1.cs
+++ calculadora-para-teste/teste-calculadora/UnitTest1.cs
@@ -1,22 +1,63 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
 namespace teste_calculadora
 {
-    public class UnitTest1
+    public class CalculatorIntegrationTests
     {
+        private readonly HttpClient _client;
+
+        public CalculatorIntegrationTests()
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7030/")
+            };
+        }
+
         [Theory]
         [InlineData("soma", 2, 3, 5)]
         [InlineData("subtracao", 5, 3, 2)]
         [InlineData("multiplicacao", 4, 3, 12)]
         [InlineData("divisao", 10, 2, 5)]
-        public void Test_Calculate(string operation, double value1, double value2, double expected)
+        [InlineData("divisao", 10, 0, double.NaN)] // Teste para divisão por zero
+        public async Task Test_Calculate(string operation, double value1, double value2, double expected)
         {
             // Arrange
-            var calculadora = new Calculadora();
+            var url = $"/calculate/{operation}/{value1}/{value2}";
 
             // Act
-            var result = calculadora.Calcular(operation, value1, value2);
+            var response = await _client.GetAsync(url);
 
             // Assert
-            Assert.Equal(expected, result);
+            if (double.IsNaN(expected))
+            {
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+            else
+            {
+                response.EnsureSuccessStatusCode();
+                var responseString = await response.Content.ReadAsStringAsync();
+                var result = JToken.Parse(responseString).Value<double>();
+                Assert.Equal(expected, result);
+            }
+        }
+
+        [Fact]
+        public async Task Test_Calculate_InvalidOperation()
+        {
+            // Arrange
+            var url = "/calculate/invalido/10/2";
+
+            // Act
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
--- calculadora-para-teste/teste-calculadora/obj/Debug/net6.0/teste-calculadora.AssemblyInfo.cs
+++ calculadora-para-teste/teste-calculadora/obj/Debug/net6.0/teste-calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste-calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abd92208d9f7e961c6674d5610f7d369c8a2464")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste-calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste-calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
